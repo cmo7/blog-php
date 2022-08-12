8ad1df057e9e699bcd1f8c52a7cc35d770469ca3
Marce Concepción <marcelinocb@gmail.com>
2022-08-12 22:29:18 +0100

    Arreglado título de login

--- public/login.php
+++ public/login.php
@@ -15,12 +15,12 @@ if (is_logged_in()) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Acceso</title>
 </head>
 
 <body>
     <?php require __DIR__ . "/components/header.php" ?>
-    <h1>Registro</h1>
+    <h1>Acceso</h1>
     <form action="/auth/controller-login.php" method="post">
         <label for="username">Nombre de usuario</label>
         <input type="text" name="username" id="username">
